1c1d6fca05ef9c5ebd5042664fe3d414ab1838ea
Nick Solter <nick.solter@sanvello.com>
2020-10-08 17:48:46 -0600

    Revert "Fixes Android 8 Crashes after Device Reboot"
    This reverts commit 55492ac79258c2c6a32c6691881ff2ef1c898292.

--- src/android/notification/receiver/AbstractRestoreReceiver.java
+++ src/android/notification/receiver/AbstractRestoreReceiver.java
@@ -26,7 +26,6 @@ package de.appplant.cordova.plugin.notification.receiver;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.os.UserManager;
 
 import org.json.JSONObject;
 
@@ -58,11 +57,8 @@ abstract public class AbstractRestoreReceiver extends BroadcastReceiver {
     public void onReceive (Context context, Intent intent) {
         String action = intent.getAction();
 
-        if (SDK_INT >= 24) {
-          UserManager um = (UserManager) context.getSystemService(UserManager.class);
-          if (um == null || um.isUserUnlocked() == false)
+        if (action.equals(ACTION_BOOT_COMPLETED) && SDK_INT >= 24)
             return;
-        }
 
         Manager mgr               = Manager.getInstance(context);
         List<JSONObject> toasts = mgr.getOptions();
